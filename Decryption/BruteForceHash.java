d92950a54c3b36fa753e93a87a7ad1377f72b0c3
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-03 16:49:49 +0200

    added .gitignore and ignored .DS_Store

--- Decryption/BruteForceHash.java
+++ Decryption/BruteForceHash.java
@@ -24,7 +24,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
-public class ForceCompareHash {
+public class BruteForceHash {
     
     public static String calculateMD5(String input){
         //Calculates the MD5 hash of the input string
