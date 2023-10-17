4fbf01e5e00e0df08ecf3e53ed841d86dd38e50a
Jonas Ullvahn <ullvahn@gmail.com>
2023-10-17 16:45:54 +0200

    update cleaning

--- MathematicPatterns/PythagoreanskTrippel.java
+++ MathematicPatterns/PythagoreanskTrippel.java
@@ -4,30 +4,13 @@ package MathematicPatterns;
 public class PythagoreanskTrippel {
     
     public static void main(String[] args) {
-        //A ArrayList for a, b and c
-        /* 
-        ArrayList<Integer> a = new ArrayList<>();
-        ArrayList<Integer> b = new ArrayList<>();
-        ArrayList<Integer> c = new ArrayList<>();
-        */
-        /* 
-        // give the list an range (1,  1000)
-        for (int i = 1; i < 1001; i++) {
-            a.add(i);
-            b.add(i);
-            c.add(i);
-        } 
-        */
-        //System.out.println(a.size());
 
-        //köra igenom och arrays och testa värdena mot varandra
         for (int a = 0; a < 950; a++) {
                       
             for (int b = 0; b < 950; b++) {
                               
                 for (int c = 0; c < 950; c++) {
                    
-
                     int sum = a + b + c;
                     if (((a*a)+(b*b) == c*c) && ((a < b) && (b < c))) {
                         
